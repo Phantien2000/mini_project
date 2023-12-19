e9db4a2fa9ab39a6c715c51446c332b77c9d601e
phantien <tien.pv181277@gmail.com>
2023-12-19 23:41:22 +0700

    test

--- src/main/java/com/example/mini_project/securityConfig/UserDetailsServiceClass.java
+++
@@ -1,6 +0,0 @@
-package com.example.mini_project.securityConfig;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public class UserDetailsServiceClass implements UserDetailsService {
-}
