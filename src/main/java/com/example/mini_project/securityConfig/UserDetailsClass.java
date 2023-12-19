d7c7830a42467fd11738650a2ada98caf4d69ece
phantien <tien.pv181277@gmail.com>
2023-12-20 00:08:23 +0700

    test3

--- src/main/java/com/example/mini_project/securityConfig/UserDetailsClass.java
+++
@@ -1,6 +0,0 @@
-package com.example.mini_project.securityConfig;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public class UserDetailsClass implements UserDetails {
-}
--- src/main/java/com/example/mini_project/securityConfig/jwt/JwtTokenUtil.java
+++
@@ -1,4 +0,0 @@
-package com.example.mini_project.securityConfig.jwt;
-
-public class JwtTokenUtil {
-}
